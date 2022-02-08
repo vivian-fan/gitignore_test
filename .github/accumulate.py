c590b4ea3eacaaf74981a05284a806b80b74e5db
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-08 11:30:30 -0800

    Update accumulate.py

--- .github/accumulate.py
+++ .github/accumulate.py
@@ -1,1 +1,9 @@
+# Now is on feature branch
 
+# Step1: read intent.yml from feature branch
+
+# Step2: merge change from master ./github/intent.yml to feature branch
+
+# Step3: accumulate new intents behind or delete new intents
+
+# Step4: merge the feature branch to master
