5d2777aabc2d59fcf073982de0fecae8f8ce11e6
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-08 15:02:42 -0800

    Update accumulate.py

--- .github/accumulate.py
+++ .github/accumulate.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import git
 import yaml
 
@@ -6,30 +8,30 @@ remote_refs = repo.remote().refs
 
 repo_heads = repo.heads
 
-print('Current branch is: ', repo.active_branch)
+print ('Current branch is: ', repo.active_branch)
 
-print('All branches are: ')
+print 'All branches are: '
 for head in repo_heads:
-    print(head.name)
+    print head.name
 
-
-print('All remote branches are: ')
+print 'All remote branches are: '
 for refs in remote_refs:
-    print(refs.name)
- 
- with open('./.github/intent.yml', 'r') as f:
-    print('internal on master ', yaml.safe_load(f))
+    print refs.name
+
+with open('./.github/intent.yml', 'r') as f:
+    print ('internal on master ', yaml.safe_load(f))
 
 # Now is on feature branch
+
 branch_name = 'feature-1'
 origin = repo.remotes.origin
 new_branch = repo.create_head(branch_name, origin.refs[branch_name])
 new_branch.checkout()
 
-print('Current branch is: ', repo.active_branch)
+print ('Current branch is: ', repo.active_branch)
 
 with open('./intent.yml', 'r') as f:
-   print('internal on feature branch ', yaml.safe_load(f))
+    print ('internal on feature branch ', yaml.safe_load(f))
 
 # Step1: read intent.yml from feature branch
 
